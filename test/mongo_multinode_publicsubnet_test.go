d07dc9b8a4dfb90d3c3115ba39786244fcfe5e6d
Mohammed Faizuddin <47523926+FaizuddinEverest@users.noreply.github.com>
2020-05-26 16:14:35 +0530

    Adding tests for checking replication status for secondary node

--- test/mongo_multinode_publicsubnet_test.go
+++ test/mongo_multinode_publicsubnet_test.go
@@ -27,9 +27,6 @@ func TestMongoDBMultiNodeInPublicSubnet(t *testing.T) {
 	terraformOptions := setupMongoInPublicSubnet(t, vars)
 	mongoPublicIPAddresses := terraform.OutputList(t, terraformOptions, "mongo_replica_ip_address")
 
-	defer terraform.Destroy(t, prerequisiteTerraformOptions)
-	defer terraform.Destroy(t, terraformOptions)
-
 	primaryIP := mongoPublicIPAddresses[0]
 	fmt.Println("MongoDB Primary IP: ", primaryIP)
 	checkMongoConnectivity(t, primaryIP)
@@ -37,4 +34,35 @@ func TestMongoDBMultiNodeInPublicSubnet(t *testing.T) {
 	secondaryIP := mongoPublicIPAddresses[1]
 	fmt.Println("MongoDB Secondary IP: ", secondaryIP)
 	checkMongoConnectivity(t, secondaryIP)
+
+	checkMongoReplication(t, primaryIP, secondaryIP)
+
+	defer terraform.Destroy(t, prerequisiteTerraformOptions)
+	defer terraform.Destroy(t, terraformOptions)
+}
+
+func checkMongoReplication(t *testing.T, primaryIp string, secondaryIp string) {
+	primaryURL := "mongodb://" + primaryIp + ":27017"
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(primaryURL))
+	assert.Nil(t, err)
+
+	database := client.Database("admin")
+
+	var result bson.M
+	cmd := bson.M{
+		"replSetGetStatus": 1,
+	}
+	err = database.RunCommand(ctx, cmd).Decode(&result)
+	assert.Nil(t, err)
+
+	replMembers := []interface{}(result["members"].(primitive.A))
+	assert.Equal(t, len(replMembers), 2)
+
+	secondaryReplStatus := replMembers[1].(primitive.M)
+
+	assert.Equal(t, secondaryReplStatus["name"], secondaryIp+":27017")
+	assert.Equal(t, secondaryReplStatus["stateStr"], "SECONDARY")
+	assert.Equal(t, secondaryReplStatus["health"], 1.0)
 }
